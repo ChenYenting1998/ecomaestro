3c9c10cb11b0b13fe05e11ee681739004d61e3f7
chenyenting1998 <tumhark@gmail.com>
2021-09-24 11:04:22 +0800

    update data description

--- R/biovolume_method.R
+++ R/biovolume_method.R
@@ -1,6 +1,7 @@
 #' @title Biovolume method
 #' @description This data documents the biovolume estimation methods
 #'              respective to each taxon.
+#'  \itemize{
 #'     \item \code{Taxon} Taxa in the recorded form.
 #'     \item \code{Note} Parsing the taxa into different estimation methods.
 #'     \item \code{Method} The methods for biovolume methods. Currently
@@ -9,4 +10,6 @@
 #'     \item \code{C_origin} The origin of conversion factors for taxa
 #'                           without published factors.
 #'     \item \code{Comment} Comment about the whole row.
+#'     }
+#'
 "biovolume_method"
